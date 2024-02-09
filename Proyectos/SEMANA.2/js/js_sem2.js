b50657d09b605ac5e2ecf8993d2659ca9200ad78
DamiArebalo <damiiarebalo@gmail.com>
2024-02-09 08:42:27 -0300

    Arranca nueva semana con nuevas herramientas y microdesafios

---
+++ Proyectos/SEMANA.2/js/js_sem2.js
@@ -0,0 +1,7 @@
+/*Te recomendamos realizar el siguiente desafío en Visual Studio Code:
+
+
+
+1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar Cuántos mundiales vivió una persona.
+
+Ten en cuenta que se juega un mundial cada 4 años y que se han jugado 21 copas del mundo.*/
--- Proyectos/SEMANA1/js/backend.js
+++ Proyectos/SEMANA1/js/backend.js
@@ -1,3 +1,5 @@
+/* VAR NO SE USA!!  
+
 let nombre = prompt("¿Como te llamas?");
 console.log(nombre)
 let edad = parseInt(prompt("¿cuantos años tenes?"));
@@ -7,7 +9,11 @@ console.log(mensaje)
 
 if(mensaje){
     console.log("ingreso del usuario "+ nombre + " de " + edad + " años");
-}
+} */
+
+let calculo = parseInt(10 + "10") 
+
+let prueba = calculo * 2
 
 
-
+console.log(prueba)
