c9d9f84838316405de09f86e29b7d1958a531dba
DamiArebalo <damiiarebalo@gmail.com>
2024-02-17 17:10:47 -0300

    dos videos mas y mas notas, cambio el ejercio extra utilizando funciones

--- Proyectos/SEMANA.2/ciclos/desafioextra.js
+++ Proyectos/SEMANA.2/ciclos/desafioextra.js
@@ -1,22 +1,45 @@
-let ciclo =confirm("Evaluamos un numero?");
 let pares = 0;
 let impares = 0;
-while(ciclo){
+
+function pruebaPar(numEntrada){
+   let prueba = (numEntrada % 2) ;
+   return(prueba)
+}
+
+function pedirNumero(){
     let entrada = parseInt(prompt("Ingrese un numero entero"));
-    let prueba = (entrada % 2) ;
-    if(prueba == 0){
-        alert("el numero "+entrada+" es PAR");
-        pares++
+    return(entrada)
+    
+}
+
+function muestra(numEntrada){
+    let entrada = pruebaPar(numEntrada) 
+    if(entrada==0){
+        alert("el numero "+numEntrada+" es PAR");
     }else{
-        alert("el numero "+entrada+" es impar");
-        impares ++
+        alert("el numero "+numEntrada+" es impar");
     }
-    console.log("Pares Parciales: "+pares+" Impares Parciales: "+impares);
+}
+
+let ciclo =confirm("Evaluamos un numero?");
+while(ciclo){
+   let entrada = pedirNumero();
+    muestra(entrada);
+    let prueba = pruebaPar(entrada) 
+    if(prueba==0){
+
+        pares++;
+    }else{
+        impares++;
+    }
     ciclo = confirm("¿Evaluamos otro numero?");
+    console.log(pares+" "+impares)
 
 }
 
 alert("Ingresaste "+pares+" N° Pares y "+impares+" N° Impares");
+
+
 
 
 
