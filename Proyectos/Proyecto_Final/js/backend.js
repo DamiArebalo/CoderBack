92867572fd0d3f6edb397827e51621d4fcc7f0db
DamiArebalo <damiiarebalo@gmail.com>
2024-02-26 10:32:25 -0300

    avance de proyecto estilos css

---
+++ Proyectos/Proyecto_Final/js/backend.js
@@ -0,0 +1,10 @@
+function openPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+
+function validateAndClose() {
+    // Validar campos de correo y contraseña aquí
+    // Si son válidos, cerrar el Pop Up
+    document.getElementById("loginPopup").style.display = "none";
+}
