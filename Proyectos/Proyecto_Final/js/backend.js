4198fb5dd94eabcb9b7eb29d467b60e091b9c4ba
DamiArebalo <damiiarebalo@gmail.com>
2024-02-27 10:31:12 -0300

    Avance con el proyecto final

--- Proyectos/Proyecto_Final/js/backend.js
+++ Proyectos/Proyecto_Final/js/backend.js
@@ -1,10 +1,55 @@
 function openPopup() {
+    document.getElementById("modalBackground").style.display = "block";
     document.getElementById("loginPopup").style.display = "block";
 }
 
-
-function validateAndClose() {
-    // Validar campos de correo y contraseña aquí
-    // Si son válidos, cerrar el Pop Up
+function closePopup() {
+    document.getElementById("modalBackground").style.display = "none";
     document.getElementById("loginPopup").style.display = "none";
 }
+
+function validateAndClose() {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+
+    if (!emailRegex.test(emailInput.value)) {
+        emailInput.classList.add("error");
+        emailError.textContent = "Ingreso incorrecto, intente otra vez o regístrese";
+    } else {
+        emailInput.classList.remove("error");
+        emailError.textContent = "";
+    }
+
+    if (!passwordRegex.test(passwordInput.value)) {
+        passwordInput.classList.add("error");
+        passwordError.textContent = "Ingreso incorrecto, intente otra vez o regístrese";
+    } else {
+        passwordInput.classList.remove("error");
+        passwordError.textContent = "";
+    }
+
+
+    if (emailRegex.test(emailInput.value) && passwordRegex.test(passwordInput.value)) {
+        closePopup();
+    }
+}
+
+function openPopupProducts() {
+    document.getElementById("modalBackgroundProducts").style.display = "block";
+    document.getElementById("productPopup").style.display = "block";
+}
+
+function closePopupProduct() {
+    document.getElementById("modalBackgroundProducts").style.display = "none";
+    document.getElementById("productPopup").style.display = "none";
+}
+
+// function addProduct() {
+//     // Lógica para agregar el producto (no implementada en este ejemplo)
+//     closePopupProduct();
+// }
--- Proyectos/Semana.3/js/JS3.js
+++ Proyectos/Semana.3/js/JS3.js
@@ -1,11 +1,67 @@
-const sumar5=(num1,num2) =>{
-    let resultado = (num1+5)*num2;
+// const sumar5=(num1,num2) =>{
+//     let resultado = (num1+5)*num2;
 
-    return(resultado)
+//     return(resultado)
+// }
+
+// let ingreso=parseInt(prompt("ingresa un numero"));
+// let ingreso2=parseInt(prompt("ingresa un numero"));
+
+// console.log(sumar5(ingreso,ingreso2));
+
+
+
+const iva = 21;
+
+
+
+//sacar la diferencia de aumento 
+function aumentoAprox(factura,lista,aumento,descuento){
+    let nuevoPrecio = lista + (lista*aumento/100);
+    let resultadoFinal= nuevoPrecio - (nuevoPrecio*descuento/100);
+
+    return(resultadoFinal-factura);
 }
 
-let ingreso=parseInt(prompt("ingresa un numero"));
-let ingreso2=parseInt(prompt("ingresa un numero"));
 
-console.log(sumar5(ingreso,ingreso2));
+//sacar el monto sin IVA para realizar ajuste
+function ajuste(factura){
+    let precioNuevo = parseInt(prompt("Cuanto le tiene que quedar"));
+    let parcial = factura - precioNuevo;
+    let resultado = parcial -(parcial*iva/100);
+
+    return(resultado)
+
+}
+
+let accion = confirm("Nueva gestion?") 
+do{
+   let tarea = confirm("aceptar=Ajuste  Cancelar = AumentoAprox");
+
+    if(tarea==false){
+        let factura = parseInt(prompt("Ultima Factura- Sin adicionales"));
+
+        let lista = parseInt(prompt("precio lista actual"))
+
+        let aumento = parseInt(prompt("Porcentaje de Aumento"));
+
+        let descuento = parseInt(prompt("descuento"));
+
+       alert("Diferencia de Aumento : "+ aumentoAprox(factura,lista,aumento,descuento))
+    }else{
+
+        let factura = parseInt(prompt("Ultima Factura- Sin adicionales"));
+
+        
+
+        alert("Ajuste : "+ajuste(factura));
+    }
+
+    accion = confirm("Nueva gestion?")
+
+}while(accion)
+
+
+
+
 
